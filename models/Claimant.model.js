c4f7c0dabe1f9ff9f44cf11a839c5d7fbaef596d
Alois Napitalai <anapitalai@gmail.com>
2021-11-16 11:12:26 +1000

    Update Claimant.model.js

--- models/Claimant.model.js
+++ models/Claimant.model.js
@@ -3,8 +3,9 @@ const  User = require('./User.model')
 
 const ClaimantSchema = mongoose.Schema({
     name: {type:String,required:true,unique:true},
+    relationship_to_deceased:{type:String,required:false,unique:true},
     sex: {type:String,required:false,unique:false},
-    date_of_brith: {type:Date,required:true,unique:false},
+    description:{type:String,required:true,unique:true},
 
     owner_id: {
         type: mongoose.Types.ObjectId,
